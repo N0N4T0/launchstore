78377f54c253e9e7a7da34fde6feb86dfe1d2676
Lucio Nonato <luciononato@hotmail.com>
2020-07-20 13:46:56 -0300

    2 commit
    estruturando pastas e montando BD

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const routes = express.Router()
 
-const instructors = require('./app/controllers/instructors')
-const members = require('./app/controllers/members')
-
-
 // Rotas
 routes.get('/', function(req, res) {
-    return res.send("Hello world")
+    return res.render("layout.njk")
 })
 
 
