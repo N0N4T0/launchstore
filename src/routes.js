71f1321d1d990b5d2ddc6b330baaa9bd8b351cf5
Lucio Nonato <luciononato@hotmail.com>
2020-07-21 12:37:45 -0300

    4 commit
    em create foi criado: rota, view e controller

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const routes = express.Router()
+const ProductController = require("../src/app/controllers/ProductController")
 
 // Rotas
 routes.get('/', function(req, res) {
     return res.render("layout.njk")
 })
 
+
+routes.get('/products/create', ProductController.create)
+
+
+routes.get('/ads/create', function(req,res){
+    return res.redirect("/products/create")
+})
 
 module.exports = routes
