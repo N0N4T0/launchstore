59de404b1817dd5a5a3e0be7ea22bd798b4f95fd
Lucio Nonato <luciononato@hotmail.com>
2020-07-24 22:06:38 -0300

    12.4 commit
    removendo miniatura. Necessário remover o conteúdo do FileList

--- public/script.js
+++ public/script.js
@@ -15,17 +15,12 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'), 
     uploadLimit: 6,
     handleFileInput(event) {
         const {files: fileList} = event.target
-        const { uploadLimit } = PhotosUpload
 
-        if (fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} fotos`)
-            event.preventDefault()
-
-            return
-        }
+        if (PhotosUpload.hasLimit(event)) return
 
         Array.from(fileList).forEach(file => {
             const reader = new FileReader()
@@ -34,19 +29,56 @@ const PhotosUpload = {
                 const image = new Image() // cria tag html <img>
                 image.src = String(reader.result)
 
-                const div = document.createElement('div')
-                div.classList.add('photo')
+                const div = PhotosUpload.getContainer(image)
 
-                div.onclick = () => alert('remover foto')
-
-                div.appendChild(image) //colocando image dentro da div
-
-                document.querySelector('#photos-preview').appendChild(div)
-
+                PhotosUpload.preview.appendChild(div)
             }
 
             reader.readAsDataURL(file)
-
         }) 
-    } 
+    },
+
+    hasLimit(event){
+        const { uploadLimit } = PhotosUpload
+        const {files: fileList} = event.target
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+
+            return true
+        }
+
+        return false
+    },
+    
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick = PhotosUpload.removePhoto
+
+        div.appendChild(image) //colocando image dentro da div
+
+        div.appendChild(PhotosUpload.getRemoveButton())
+
+        return div
+    },
+
+    getRemoveButton() {
+        const button = document.createElement('i')
+
+        button.classList.add('material-icons')
+        button.innerHTML = "close"
+
+        return button
+    },
+
+    removePhoto(event) {
+        const photoDiv = event.target.parentNode
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index = photosArray.indexOf(photoDiv)
+
+        photoDiv.remove()
+    }
 }
