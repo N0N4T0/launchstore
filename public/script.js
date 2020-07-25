baf76f70b00d9e681b8711d5ee20835913981e18
Lucio Nonato <luciononato@hotmail.com>
2020-07-25 00:59:41 -0300

    12.5 commit
    estruturando um script para que se possa remover elmentos de um array de files, por padrão o construtor FileList é só de leitura.

--- public/script.js
+++ public/script.js
@@ -15,14 +15,19 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    input: "",
     preview: document.querySelector('#photos-preview'), 
     uploadLimit: 6,
+    files: [],
     handleFileInput(event) {
         const {files: fileList} = event.target
+        PhotosUpload.input = event.target
 
         if (PhotosUpload.hasLimit(event)) return
 
         Array.from(fileList).forEach(file => {
+            PhotosUpload.files.push(file)
+            
             const reader = new FileReader()
 
             reader.onload = () => {
@@ -35,12 +40,13 @@ const PhotosUpload = {
             }
 
             reader.readAsDataURL(file)
-        }) 
+        })
+        
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
     },
 
     hasLimit(event){
-        const { uploadLimit } = PhotosUpload
-        const {files: fileList} = event.target
+        const { uploadLimit, input: fileList } = PhotosUpload
 
         if (fileList.length > uploadLimit) {
             alert(`Envie no máximo ${uploadLimit} fotos`)
@@ -50,6 +56,15 @@ const PhotosUpload = {
         }
 
         return false
+    },
+
+    getAllFiles() {
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+        // console.log(dataTransfer)
+        return dataTransfer.files
     },
     
     getContainer(image) {
@@ -75,9 +90,12 @@ const PhotosUpload = {
     },
 
     removePhoto(event) {
-        const photoDiv = event.target.parentNode
+        const photoDiv = event.target.parentNode // i @ <div class="photo">
         const photosArray = Array.from(PhotosUpload.preview.children)
         const index = photosArray.indexOf(photoDiv)
+
+        PhotosUpload.files.splice(index, 1)
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
 
         photoDiv.remove()
     }
