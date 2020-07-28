914ca5a4e6b71cf4e59434e5c2bb6ff3eb46107b
Lucio Nonato <luciononato@hotmail.com>
2020-07-28 11:22:25 -0300

    16 commit
    lógica de exclusão de imagens vindas do backend

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db')
+const fs = require('fs')
 
 module.exports = {
     create({filename, path, product_id}){
@@ -21,4 +22,16 @@ module.exports = {
        return db.query(query, values)      
 
     },
+
+    async delete(id) {
+
+        const result = await db.query(`SELECT * FROM files WHERE id = $id`, [id])
+        const file = result.rows[0]
+
+        fs.unlinkSync(file.path)
+
+        return db.query (`
+            DELETE FROM files WHERE id = $1
+        `, [id])
+    }
 }
