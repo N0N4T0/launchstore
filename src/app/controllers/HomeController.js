4b7067e97ab152b865dfe414511a6f8303acf28f
Lucio Nonato <luciononato@hotmail.com>
2020-08-09 21:45:14 -0300

    22.1 commit
    filtrando items do array

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -9,7 +9,27 @@ module.exports = {
         let results = await Product.all()
         const products = results.rows[0]
 
-        if(!products) return res.send("Priducts not found!")
+        if(!products) return res.send("Products not found!")
+
+        async function getImage(productId) {
+            let results = await Product.files(product.id)
+            const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+
+            return files[0]
+        }
+
+        const productsPromise = products.map(product => {
+            product.img = await getImage(product)
+            product.oldPrice = formatPrice(product.old_price)
+            product.price = formatPrice(product.price)
+
+            return product
+        }).filter((product, index) => index > 2 ? false : true )
+
+        const lastAdded = await Promise.all(productsPromise)
+
+        return res.render("home/index", { products: lastAdded })
+
     }
 
 }
