12f1c9ce4d942ac4eaf57df9a8bc0afce6334226
Lucio Nonato <luciononato@hotmail.com>
2020-08-11 16:12:28 -0300

    26.1 commit
    organizando categorias e filtros

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -95,7 +95,7 @@ module.exports = {
         if(category) {
             filterQuery = `
             ${filterQuery}
-            products.category_id = ${category_id}
+            products.category_id = ${category}
             AND`
         }   
         
@@ -111,7 +111,6 @@ module.exports = {
             FROM products
             LEFT JOIN categories ON (categories.id = products.category_id)
             ${filterQuery}
-            GROUP BY categories.name
         `
 
         return db.query(query)
