3c087a3f7d7ef38d6d5b80de1951eed81f10dbce
Lucio Nonato <luciononato@hotmail.com>
2020-09-10 23:52:16 -0300

    38.1 commit
    LOGIN FINALIZADO

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,16 @@
 module.exports = {
     loginForm(req, res){
-        return res.render("session/index")
+        return res.render("session/login")
+    },
+
+    login(req, res){
+        //verificar se o usuário está cadastrado
+        //verificar se o password bate
+        //depois colocar o usuário no req.session
+
+        req.session.userId = req.user.id
+
+        return res.redirect("/users")
     },
 
     logout(req, res) {
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,11 +4,13 @@ const routes = express.Router()
 const SessionController = require("../app/controllers/SessionController")
 const UserController = require("../app/controllers/UserController")
 
-const Validator = require("../app/validators/users")
+const UserValidator = require("../app/validators/users")
+const SessionValidator = require("../app/validators/session")
+
 
 // // Login/Logout
 routes.get('/login', SessionController.loginForm)
-// routes.post('/login', SessionController.login)
+routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
 
@@ -21,10 +23,10 @@ routes.post('/logout', SessionController.logout)
 
 // user register UserController
 routes.get('/register', UserController.registerForm)
-routes.post('/register', Validator.post, UserController.post)
+routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/', Validator.show, UserController.show)
-routes.put('/', Validator.update, UserController.update)
+routes.get('/', UserValidator.show, UserController.show)
+routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
 
