a0e317933cf6200726fe01e5e95d335f443264f1
Lucio Nonato <luciononato@hotmail.com>
2020-09-12 00:42:45 -0300

    42.1 commit
    Instalando NODEMAILER e usando mailtrap para visualizar o email chegando.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
-const crypto = require('crypto')
 const User = require('../models/User')
+
+const crypto = require('crypto')
+const mailer = require("../../lib/mailer")
 
 module.exports = {
     loginForm(req, res){
@@ -26,24 +28,49 @@ module.exports = {
         return res.render("session/forgot-password")
     },
 
-    forgot(req, res) {
+    async forgot(req, res) {
         const user = req.user
 
-        //token para esse usuário
-        const token = crypto.randomBytes(20).toString("hex")
+        try {
+            //token para esse usuário
+            const token = crypto.randomBytes(20).toString("hex")
 
-        //criar uma expiração do token
-        let now = new Date()
-        now = now.setHours(now.getHours() + 1)
+            //criar uma expiração do token
+            let now = new Date()
+            now = now.setHours(now.getHours() + 1)
 
-        await User.update(user.id, {
-            reset_token: token,
-            reset_token_expires: now
-        })
+            await User.update(user.id, {
+                reset_token: token,
+                reset_token_expires: now
+            })
 
-        //enviar um email com um link de recuperação de senha
-        //avisar o usuário que enviamos o e-mail
+            //enviar um email com um link de recuperação de senha
+            await mailer.sendMail({
+                to: user.email,
+                from: 'no-reply@launchstore.com.br',
+                subject: 'Recuperação de senha',
+                html: `<h2>Perdeu a chave?</h2>
+                <p>Não se preocupe, clique no link abaixo para recuperar sua senha</p>
+                <p>
+                    <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
+                        RECUPERAR SENHA
+                    </a>
+                </p>
+                `
+            })
 
+            //avisar o usuário que enviamos o e-mail
+            return res.render("session/forgot-password", {
+                success: "Verifique seu email para resetar sua senha"
+            })
+
+        } catch (err) {
+            console.error(err)
+            return res.render("session/forgot-password", {
+                error: "Erro inesperado. Tente novamente"
+            })
+        }
+        
     }
 
 }
